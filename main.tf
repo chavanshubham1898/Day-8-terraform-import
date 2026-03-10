9d80a335b3463e3ef70411389d9fb14bdba7e9aa
chavanshubham1898 <chavanshubham1898@gmail.com>
2026-03-10 18:33:43 +0530

    remove data block

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 resource "aws_instance" "FCT" {
   ami = var.image
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.sg.id ]
+  vpc_security_group_ids = [ aws_security_group.sg.id]
   tags = {
     Name = "Terraform-instance"
   }
 }
 
-data "aws_security_group" "sg" {
+resource "aws_security_group" "sg" {
   name = "Terraform-sg"
   vpc_id = "vpc-07da03033c1eafc3c"
 }
